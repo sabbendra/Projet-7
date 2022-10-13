68ecca9be74f02ef3c56f0bd6ed672142ea1b096
sabrina bendraoua <sabbendra@gmail.com>
2022-10-13 23:42:13 +0200

    responsive header

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Banner from "../components/Banner.js";
 import Cards from '../components/Cards.js';
+import "../css/body.css";
 
 
 export class Home extends Component {
